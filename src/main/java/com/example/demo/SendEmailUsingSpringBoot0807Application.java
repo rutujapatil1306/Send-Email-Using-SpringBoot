42fb511e4ad08fce1742567f8d91d2907fb5bcdc
Rutuja <rutujapatil1306@gmail.com>
2025-07-08 23:09:50 +0530

    First Commit

---
+++ src/main/java/com/example/demo/SendEmailUsingSpringBoot0807Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailUsingSpringBoot0807Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailUsingSpringBoot0807Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/SendEmailUsingSpringBoot0807ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailUsingSpringBoot0807ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
